5f12df966c6bd628a048841dc0bf9afe4203fab7
Ricardo Costa Alves dos Santos <ricardo_cas4@hotmail.com>
2025-08-02 22:47:16 -0300

    :sparkles:

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAREL_4sP8ANwqw9m-WlOOXHuKtazG9IXY",
-    authDomain: "converta-e-planeje.firebaseapp.com",
-    projectId: "converta-e-planeje",
-    storageBucket: "converta-e-planeje.firebasestorage.app",
-    messagingSenderId: "1093053743340",
-    appId: "1:1093053743340:web:b21127c539ea5c3eb6e5c9",
-    measurementId: "G-YKZP39G9GZ"
+    apiKey: process.env.VITE_FIREBASE_API_KEY,
+    authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VITE_FIREBASE_APP_ID,
+    measurementId: process.env.G_ANALYTICS_ID
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
